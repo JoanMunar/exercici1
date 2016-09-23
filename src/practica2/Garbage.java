30ddf9eb84735b3307e208c5bf20f915016d4bcc
jmunarb <munar.bestard.joan@gmail.com>
2016-09-23 17:25:03 +0200

    Introducció Garbage collector

---
+++ src/practica2/Garbage.java
@@ -0,0 +1,25 @@
+package practica2;
+
+/**
+ * Created by jmunarb on 23/09/16.
+ */
+public class Garbage {
+
+    public static void main(String[] args) {
+
+        Garbage potato = new Garbage();
+
+        potato = null;
+
+        System.gc();
+
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable{
+
+        System.out.println("Hola");
+
+    }
+}
